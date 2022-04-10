d4001dc0a07d41bc91edcba4fcadd05025d2bf9b
Denis Shubin <denis.shubin@gmail.com>
2022-04-10 19:29:14 +0300

    feat: add functionality

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,17 +1,20 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './Counter.module.css'
+import {Button} from './components/Button';
 
 export type TextType = `enter values and press 'set'` | `Incorrect value!`
 
 export const Counter = () => {
-    const [startValue, setStartValue] = useState(0)
-    const [maxValue, setMaxValue] = useState(5)
-    const [count, setCount] = useState<number | null>(null)
-    const [error, setError] = useState(false)
-    const [disabled, setDisabled] = useState(false)
+    const [startValue, setStartValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(5)
+    const [count, setCount] = useState<number>(startValue)
+    const [startValueError, setStartValueError] = useState(false)
+    const [maxValueError, setMaxValueError] = useState(false)
+    const [disabled, setDisabled] = useState(true)
+    const [disabledInc, setDisabledInc] = useState(true)
+    const [disabledReset, setDisabledReset] = useState(true)
     const [text, setText] = useState<TextType>(`enter values and press 'set'`)
-
-
+    const [isPreview, setIsPreview] = useState(true)
     useEffect(() => {
         let valueStartValueAsString = localStorage.getItem('startValue')
         if (valueStartValueAsString) {
@@ -27,57 +30,91 @@ export const Counter = () => {
 
     useEffect(() => {
         localStorage.setItem('startValue', JSON.stringify(startValue))
-    }, [startValue])
-
-    useEffect(() => {
         localStorage.setItem('maxValue', JSON.stringify(maxValue))
-    }, [maxValue])
+    })
 
     const setHandler = () => {
-        if (maxValue <= startValue) {
-            setError(true)
-            setDisabled(true)
-            setText(`Incorrect value!`)
-        } else {
-            setError(false)
-            setDisabled(false)
-            setCount(startValue)
-        }
         localStorage.setItem('startValue', JSON.stringify(startValue))
         localStorage.setItem('maxValue', JSON.stringify(maxValue))
         setCount(startValue)
+        setIsPreview(false)
+        setDisabled(true)
+        setDisabledInc(false)
+        setDisabledReset(false)
     }
 
     const incHandler = () => {
-        if (count && count < maxValue) {
-            setCount(state => state && state + 1)
+        if (count < maxValue) {
+            setCount(state => state + 1)
         }
     }
-
     const resetHandler = () => {
         setCount(startValue)
+        setDisabled(false)
+        setIsPreview(true)
+        setDisabledInc(true)
     }
 
     const onChangeMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        if (maxValue <= startValue) {
-            setError(true)
-            // setDisabled(true)
-            setMaxValue(JSON.parse(e.currentTarget.value))
-        } else {
-            setError(false)
-            // setDisabled(false)
-            setMaxValue(JSON.parse(e.currentTarget.value))
+        const isIncorrectValue = startValue === +e.currentTarget.value || +e.currentTarget.value < 0 || startValue > +e.currentTarget.value
+        if (isIncorrectValue) {
+
+            setStartValueError(true)
+            setMaxValueError(true)
+            setText('Incorrect value!')
+            setDisabled(true)
+        } else if (+e.currentTarget.value >= 0 && startValue >= 0) {
+
+            setStartValueError(false)
+            setMaxValueError(false)
+            setText(`enter values and press 'set'`)
+            setDisabled(false)
+        } else if (startValue < 0) {
+
+            setStartValueError(true)
+            setMaxValueError(false)
+            setDisabled(true)
         }
+        setDisabledReset(true)
+        setMaxValue(+e.currentTarget.value)
     }
 
     const onChangeStartValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setStartValue(JSON.parse(e.currentTarget.value))
+        const isIncorrectValue = +e.currentTarget.value === maxValue || maxValue < 0 || +e.currentTarget.value > maxValue
+        if (isIncorrectValue) {
+            console.log('isIncorrectValue')
+            setStartValueError(true)
+            setMaxValueError(true)  ///!
+            setText('Incorrect value!')
+            setDisabled(true)
+        } else if (+e.currentTarget.value >= 0 && maxValue >= 0) {
+            setStartValueError(false)
+            setMaxValueError(false) ////!
+            setText(`enter values and press 'set'`)
+            setDisabled(false)
+        } else if (+e.currentTarget.value < 0) {
+            setStartValueError(true)
+            setMaxValueError(false)
+            setDisabled(true)
+        }
+        setDisabledReset(true)
+        setStartValue(+e.currentTarget.value)
     }
 
-    const incDisabled = count === maxValue || count === null
-    const resetDisabled = count === startValue || count === null
+    const incDisabled = disabledInc || count === maxValue
+    const resetDisabled = disabledReset || count === startValue
 
-    const scoreboardClassname = count === maxValue ? `${s.counterBoard} ${s.red}` : `${s.counterBoard}` && count === null ? `${s.counterBoard} ${s.textSize}` : `${s.counterBoard}`
+    const scoreboardClassname = count === maxValue ? `${s.counterBoard} ${s.red}` : `${s.counterBoard}`
+
+    const displayTextClassName = text === `Incorrect value!` ? `${s.counterBoard} ${s.textError}` : `${s.counterBoard} ${s.text}`
+
+    const startValueActive = startValueError ? s.errorInput : s.defaultInput
+    const maxValueActive = maxValueError ? s.errorInput : s.defaultInput
+
+    const isViewText = isPreview || startValue < 0 || maxValue < 0
+    console.log('startValue', startValue)
+    console.log('maxValue', maxValue)
+
 
     return (
         <div className={s.wrapper}>
@@ -85,14 +122,14 @@ export const Counter = () => {
                 <div className={s.settingsBlock}>
                     <div className={s.settingsBlockInput}>
                         <span>max value:</span>
-                        <input onChange={onChangeMaxValueHandler}
+                        <input className={maxValueActive} onChange={onChangeMaxValueHandler}
                                type={'number'}
                                value={maxValue}
                         />
                     </div>
                     <div className={s.settingsBlockInput}>
                         <span>start value:</span>
-                        <input onChange={onChangeStartValueHandler}
+                        <input className={startValueActive} onChange={onChangeStartValueHandler}
                                type={'number'}
                                value={startValue}
                         />
@@ -100,25 +137,37 @@ export const Counter = () => {
                 </div>
                 <div className={s.buttonsBlock}>
                     <div>
-                        <button className={s.button}
-                                onClick={setHandler}
+                        <Button title={'set'}
+                                callback={setHandler}
+                                className={s.button}
                                 disabled={disabled}
-                        >set
-                        </button>
+                        />
                     </div>
                 </div>
             </div>
             <div className={s.counter}>
-                <div className={scoreboardClassname}>
-                    {count === null ? text : count}
-                </div>
+                {isViewText
+                    ?
+                    <div className={displayTextClassName}>
+                        {text}
+                    </div>
+                    : <div className={scoreboardClassname}>
+                        {count}
+                    </div>}
+
                 <div className={s.buttonsBlock}>
                     <div>
-                        <button disabled={incDisabled} className={s.button} onClick={incHandler}>inc</button>
+                        <Button callback={incHandler}
+                                className={s.button}
+                                disabled={incDisabled}
+                                title={'inc'}
+                        />
                     </div>
                     <div>
-                        <button disabled={resetDisabled} className={s.button} onClick={resetHandler}>reset
-                        </button>
+                        <Button title={'reset'}
+                                disabled={resetDisabled}
+                                className={s.button}
+                                callback={resetHandler}/>
                     </div>
                 </div>
             </div>
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type PropsType = {
+    title: string
+    callback: () => void
+    className: string
+    disabled: boolean
+}
+
+export const Button: React.FC<PropsType> = ({title, callback, className, ...props}) => {
+    return (
+        <button onClick={callback} className={className} disabled={props.disabled}>{title}</button>
+    );
+};
+
